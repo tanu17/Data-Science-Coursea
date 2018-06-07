a51379854a38f3bbec3e30b04483c017a5027068
Shantanu Sharma <shantanu1798@gmail.com>
2018-06-07 17:34:15 +0800

    Update galtonMUfinder.R

--- galtonMUfinder.R
+++ galtonMUfinder.R
@@ -2,8 +2,9 @@ library(manipulate)
 library(ggplot2)
 myHist <- function(mu){
   mse <- mean(( galton$child -mu)^2)
-  g<- ggplot(galton,aes(x=child)) + geom_histogram(fill="salmon", colour= "black", binwidth = 1)
-  g <- g + ggtitle(paste("mu = ",mu, " MSE = ", round(mse,3), sep = ""))
+  g <-gplot(galton,aes(x=child)) + geom_histogram(fill="salmon", colour= "black", binwidth = 1)
+  g <-g+ geom_vline(xintercept = mu, size=2)
+  g <-g + ggtitle(paste("mu = ",mu, " MSE = ", round(mse,3), sep = ""))
   g
 }
 manipulate(myHist(mu), mu = slider(62, 72, step= 0.25))
