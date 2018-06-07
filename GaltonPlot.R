9285b758c28d9e83cf903d0725d295c09bb38ffb
Shantanu Sharma <shantanu1798@gmail.com>
2018-06-07 17:14:40 +0800

    Update GaltonPlot.R

--- GaltonPlot.R
+++ GaltonPlot.R
@@ -1,7 +1,4 @@
-library(UsingR)
-data(galton)
-library(reshape)
-long<- melt(galton)
+library(UsingR);data(galton);library(reshape);long<- melt(galton)
 g <- ggplot(long,aes(x=value,fill=variable))
 g <- g + geom_histogram(color="black", binwidth=1)
 g <- g + facet_grid(. ~ variable)
