3ba4c97ebb20b864d7211b9dee43ab29aa751677
Shantanu Sharma <sharma@horangi.com>
2018-06-07 17:08:03 +0800

    Dalton's height distribution

---
+++ daltonPlot.R
@@ -0,0 +1,8 @@
+library(UsingR)
+data(galton)
+library(reshape)
+long<- melt(galton)
+g <- ggplot(long,aes(x=value,fill=variable))
+g <- g + geom_histogram(color="black", binwidth=1)
+g <- g + facet_grid(. ~ variable)
+g
