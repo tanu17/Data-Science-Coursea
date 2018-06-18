9cf810ceb8323a0990f051efee2e4386139f6a9c
Shantanu Sharma <shantanu1798@gmail.com>
2018-06-18 12:59:54 +0800

    Comments added for reading

--- Regression/Parallel Regression/Linear regression/speed_lm.r
+++ Regression/Parallel Regression/Linear regression/speed_lm.r
@@ -5,14 +5,20 @@
 set.seed(2012)
 n = 10000
 m = 1000
+# runif: random values from uniform distribution
 S = matrix(2 * runif(n * m), n, m)
 y = rnorm(n)
+# rnorm: random values from normal distribution 
 
 # Do the computations
 t0 = proc.time()[1]
+# t0 marks the staring time
 
 beta = rep(0, m)
+# initializing beta vector as (0,0,0,0,0...)
+
 for(i in 1:m){
+  # generating a linear mode based upon one SNPs having n states. Linear model provides intercept and slpoe
   mod = lm(y ~ S[,i])
   beta[i] = mod$coeff[2]
 }
