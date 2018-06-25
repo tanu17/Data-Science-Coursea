13fe2689b3cbe2e4f4d5aa2f34e9cb4e77f52258
Shantanu Sharma <shantanu1798@gmail.com>
2018-06-25 10:21:22 +0800

    Update speed_lsfit.r

--- Regression/Parallel Regression/Linear regression/speed_lsfit.r
+++ Regression/Parallel Regression/Linear regression/speed_lsfit.r
@@ -1,7 +1,10 @@
 # Check the speed of lsfit() in loop
 # Karolina Sikorska and Paul Eilers, 2012
 
+# Least Square is faster than linear modela
+
 # Simulate data
+# set.seed() is a PRG used to provide reproducable random data points
 set.seed(2012)
 n = 10000
 m = 1000
@@ -13,6 +16,7 @@ t0 = proc.time()[1]
 
 beta = rep(0, m)
 for (i in 1:m) {
+  # only change in code
   mod = lsfit(S[, i], y)
   beta[i] = mod$coeff[2]
 }
