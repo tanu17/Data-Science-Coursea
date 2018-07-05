2f8e00a1c671d50f1dd829f8a915599993bf2791
Shantanu Sharma <shantanu1798@gmail.com>
2018-07-05 19:40:17 +0800

    Delete sampler.R

--- Basic R codes/MSF course/sampler.R
+++
@@ -1,7 +0,0 @@
-sample(c("A","G","C","T"),10, replace= TRUE)
-# c denotes the set to choose from, next number denotes number of samples to draw
-
-sample(1:52, 10)
-# 1:52 is range of integers from [1,52], including both limits unlike py or c++
-
-rnorm(100,0,1)
