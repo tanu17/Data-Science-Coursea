1478b831cccfa40f102fae346b8a12f3d4c441f1
Shantanu Sharma <shantanu1798@gmail.com>
2018-07-09 20:21:27 +0800

    Update conditionsAndLoops.R

--- Basic R codes/MFE course/conditionsAndLoops.R
+++ Basic R codes/MFE course/conditionsAndLoops.R
@@ -32,4 +32,18 @@ for ( i in seq_along(v) ){
 for ( i in v){
   print(i)}
 # Example 4: Matrix multiplication using for loop
+m= 2
+n= 3
+p =3
 
+m1 <- matrix(runif(m*n), m, n)
+m2 <- matrix(runif(n*p), n, p)
+m3 <- matrix(0, m, p)
+
+for(i in 1:m){
+  for(j in 1:p){
+    m3[i,j]<- sum(m1[i,]*m2[,j])
+  }
+}
+print(all(as.vector(m1%*%m2 == m3)))  # TEST1
+print(all(abs(as.vector(m1%*%m2 - m3))< 1e-8))	# TEST2
