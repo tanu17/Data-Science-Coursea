e454e81988dfddf7decefa480066b21cc87a6e82
Shantanu Sharma <shantanu1798@gmail.com>
2018-07-09 19:25:42 +0800

    Create plottingData.R

---
+++ Basic R codes/MFE course/plottingData.R
@@ -0,0 +1,16 @@
+?plot
+x <- rnorm(1000)
+y <- rnorm(1000)
+plot(X,Y, type="p")
+plot(X,Y, type="p")
+plot(X,Y, type="l")
+hist(distribution)
+plot(X,Y, pch=1/2/3/4)
+
+# overlap two data sets in same graph and then plot something
+par(new=T)
+plot(distri2)
+
+# restricting data window
+par(mfcol= c(2,1))
+par(mfrow= c(4,5))
