aea57c748aee8d75640be8cfb56e871cd1611813
Shantanu Sharma <shantanu1798@gmail.com>
2018-07-09 19:40:05 +0800

    Create conditionsAndLoops.R

---
+++ Basic R codes/MFE course/conditionsAndLoops.R
@@ -0,0 +1,26 @@
+# 
+#	If statement
+#
+aStr<- "R lang"
+
+if (aStr == "pythonLang"){
+
+}else if ( ) {
+
+} else {
+
+}
+
+#
+#	For Loop
+# (includes both a and b in the values in a:b)
+#
+for ( i in 1:10){
+
+}
+
+# Example:
+for ( i in 1:10){
+i<-i+1
+print(i)
+}
